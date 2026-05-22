422e71cb2c2b9a40f0f1ae0ff8c14ed8b965050d
Jericho Varde <vardejericho@gmail.com>
2026-05-22 09:20:06 +0800

    feat: adjust padding for Lunas project page section for improved layout

--- src/app/work/lunas/page.jsx
+++ src/app/work/lunas/page.jsx
@@ -33,7 +33,7 @@ export default function LunasPage() {
         </div>
       </section>
 
-      <section className="bg-[#111111] px-6 pb-28 md:px-16 lg:px-24 lg:pb-36">
+      <section className="bg-[#111111] px-6 pb-28 md:pl-24 md:pr-16 lg:pl-32 lg:pr-24 lg:pb-36">
         <div className="mx-auto w-full max-w-[1220px] overflow-hidden rounded-[24px] border border-white/10 bg-[#0C0C0C] shadow-[0_24px_80px_rgba(0,0,0,0.55)]">
           <img
             src="/projects/lunas/team-otso-otso.jpg"
